3b4068e02f21dc9a703b70012552ca73e4231b16
Kiran3232 <37551543+Kiran3232@users.noreply.github.com>
2020-03-14 23:11:41 +0530

    Initial Setup | First Push

---
+++ src/main/java/com/animerepo/AnimerepoApplication.java
@@ -0,0 +1,13 @@
+package com.animerepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimerepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimerepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animerepo/AnimerepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animerepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimerepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
