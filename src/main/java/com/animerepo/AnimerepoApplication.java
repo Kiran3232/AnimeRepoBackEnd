3161a4be3832440fd33c14e9dc8df19c4eeced1f
Kiran3232 <37551543+Kiran3232@users.noreply.github.com>
2020-03-14 23:31:58 +0530

    Error Handler Setup | AirBrake

--- src/main/java/com/animerepo/AnimerepoApplication.java
+++ src/main/java/com/animerepo/AnimerepoApplication.java
@@ -2,12 +2,23 @@ package com.animerepo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.airbrake.javabrake.Notifier;
 
 @SpringBootApplication
 public class AnimerepoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimerepoApplication.class, args);
+
+		int projectId = 263374;
+		String projectKey = "3c53aa3751f0e168ad35bf696340ee8c";
+		Notifier notifier = new Notifier(projectId, projectKey);
+
+//		try {
+//			int i = 1 / 0;
+//		} catch (ArithmeticException e) {
+//			notifier.report(e);
+//		}
 	}
 
 }
