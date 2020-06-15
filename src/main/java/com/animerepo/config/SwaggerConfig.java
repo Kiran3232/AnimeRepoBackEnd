52a82ebf576657f4c78eb6da81ac5aac941255fc
Kiran3232 <37551543+Kiran3232@users.noreply.github.com>
2020-06-15 15:13:12 +0530

    Update SwaggerConfig.java

--- src/main/java/com/animerepo/config/SwaggerConfig.java
+++ src/main/java/com/animerepo/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.animerepo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,6 +15,7 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.animerepo")).paths(PathSelectors.any()).build();
 	}
 }
